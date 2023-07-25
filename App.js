a385ddfacad8738d81ec61bb3ec762bc3647897b
Your Name <psoyeon0127@naver.com>
2023-07-25 20:53:04 +0900

    event control

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);    
+  }
+
+  function addGoalHandler(enteredText) {
+    
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!'/>
-        <Button title='Add Goal'/>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -15,18 +30,27 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
- appContainer: {
-  padding: 50
- },
- inputContainer: {
-  flexDirection: 'row',
-  justifyContent: 'space-between'
- },
- textInput: {
-  borderWidth: 1,
-  borderColor: '#cccccc',
-  width: '80%',
-  marginRight: 8, // TextInput의 바깥쪽 여백
-  padding: 8 // TextInput의 안쪽 여백
- }
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8, // TextInput의 바깥쪽 여백
+    padding: 8, // TextInput의 안쪽 여백
+  },
+  goalsContainer: {
+    flex: 4,
+  },
 });

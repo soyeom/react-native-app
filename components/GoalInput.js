4eca6bd589396bc31cd463d49d8f626e0f13e5ee
Your Name <psoyeon0127@naver.com>
2023-07-30 01:28:54 +0900

    image components

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,65 +1,76 @@
 import { useState } from "react";
-import { StyleSheet, View, TextInput, Button, Modal } from "react-native";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Modal,
+  Image,
+} from "react-native";
 
 function GoalInput(props) {
-    const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [enteredGoalText, setEnteredGoalText] = useState("");
 
-    function goalInputHandler(enteredText) {
-        setEnteredGoalText(enteredText);
-    }
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
 
-    function addGoalHandler() {
-        props.onAddGoal(enteredGoalText);
-        setEnteredGoalText('');
-        //새로운 목표를 추가할 때마다 빈칸으로 만듬
-    }
+  function addGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText("");
+    //새로운 목표를 추가할 때마다 빈칸으로 만듬
+  }
 
-    return (
-        <Modal visible={props.visible} animationType="slide">
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.textInput}
-                    placeholder="Your course goal!"
-                    onChangeText={goalInputHandler}
-                    value={enteredGoalText}
-                />
-                <View style={styles.buttonContainer}>
-                    <View style={styles.button}>
-                        <Button title="Add Goal" onPress={addGoalHandler} />
-                    </View>
-                    <View style={styles.button}>
-                        <Button title='Cancel' onPress={props.onCancel} />
-                    </View>
-                </View>
-            </View>
-        </Modal>
-    );
+  return (
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <Image style={styles.image} source={require('..//assets/images/goal.png')} />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Add Goal" onPress={addGoalHandler} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" onPress={props.onCancel} />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
 }
 
 export default GoalInput;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        flex: 1,
-        justifyContent: "center", //모든 콘텐츠가 중간에 모인다
-        alignItems: "center",
-        marginBottom: 24,
-        padding: 16, // 입력란 바깥쪽 여백
-        borderBottomWidth: 1,
-        borderBottomColor: "#cccccc",
-    },
-    textInput: {
-        borderWidth: 1,
-        borderColor: "#cccccc",
-        width: "100%",
-        padding: 8, // TextInput의 안쪽 여백
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        marginTop: 16 //텍스트 입력란과 버튼 사이의 여백
-    },
-    button: {
-        width: 100, //버튼 크기
-        marginHorizontal: 8 //버튼 사이 여백
-    }
+  inputContainer: {
+    flex: 1,
+    justifyContent: "center", //모든 콘텐츠가 중간에 모인다
+    alignItems: "center",
+    padding: 16, // 입력란 바깥쪽 여백
+    backgroundColor: '#311b6b'
+  },
+  image: {
+    width:  100,
+    height: 100,
+    margin: 20,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "100%",
+    padding: 8, // TextInput의 안쪽 여백
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 16, //텍스트 입력란과 버튼 사이의 여백
+  },
+  button: {
+    width: 100, //버튼 크기
+    marginHorizontal: 8, //버튼 사이 여백
+  },
 });

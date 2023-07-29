8a78d5f39421db12d3db6266dd201e1c56fc20d8
Your Name <psoyeon0127@naver.com>
2023-07-29 16:36:30 +0900

    add water effect

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,26 +1,33 @@
-import { StyleSheet, View, Text, Pressable } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
-    //bind는 나중에 실행할 함수를 미리 조정할 수 있게 한다.
-    return (
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-            <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{props.text}</Text>
-            </View>
+  //bind는 나중에 실행할 함수를 미리 조정할 수 있게 한다.
+  return (
+    <View style={styles.goalItem}>
+        <Pressable
+            android_ripple={{ color: "#210644" }}
+            onPress={props.onDeleteItem.bind(this, props.id)}
+            style = {({pressed}) => pressed && styles.pressedItem}
+        >
+        <Text style={styles.goalText}>{props.text}</Text>
         </Pressable>
-    );
+    </View>
+  );
 }
 
 export default GoalItem;
 
 const styles = StyleSheet.create({
-    goalItem: {
-        margin: 8,
-        padding: 8,
-        borderRadius: 6,
-        backgroundColor: "#5e0acc",
-    },
-    goalText: {
-        color: "white",
-    },
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  pressedItem: {
+    opacity: 0.5
+  },
+  goalText: {
+    color: "white",
+    padding: 8,
+  },
 });

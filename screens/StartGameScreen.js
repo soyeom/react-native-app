afabf7651a2dff17e5b741766bc4dcc4ee3398e2
Your Name <psoyeon0127@naver.com>
2023-08-01 21:43:12 +0900

    textinput styling

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,11 +1,16 @@
 //맞혀야 할 숫자를 고르고 게임을 시작하는 화면
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
     return (
-        <View>
-            <TextInput />
+        <View style={styles.inputContainer}>
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2}
+                keyboardType="number-pad"
+                autoCapitalize="none"
+            />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
@@ -13,3 +18,30 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        //flex: 1, 스타일링 객체가 가능한 많은 공간 차지
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24, //양 옆에 간격
+        backgroundColor: '#72063c',
+        borderRadius: 8,
+        elevation: 4, //안드로이드에서 그림자 넣는 프로퍼티
+        shadowColor: 'black', //ios에서 그림자 넣는 프로퍼티
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.3
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        textAlign: 'center',
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold'
+    },
+})
